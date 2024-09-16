c52df258b7fa27b8ca71b00618772a67b0059300
erictsaii <erictsai90@gmail.com>
2024-09-16 17:40:07 +0800

    feat: refine algo

--- lab1/lab1.cc
+++ lab1/lab1.cc
@@ -1,19 +1,31 @@
-#include <assert.h>
-#include <stdio.h>
 #include <math.h>
+#include <mpi.h>
+#include <stdio.h>
 
 int main(int argc, char** argv) {
-	if (argc != 3) {
-		fprintf(stderr, "must provide exactly 2 arguments!\n");
-		return 1;
-	}
-	unsigned long long r = atoll(argv[1]);
-	unsigned long long k = atoll(argv[2]);
-	unsigned long long pixels = 0;
-	for (unsigned long long x = 0; x < r; x++) {
-		unsigned long long y = ceil(sqrtl(r*r - x*x));
-		pixels += y;
-		pixels %= k;
-	}
-	printf("%llu\n", (4 * pixels) % k);
+    unsigned long long r = atoll(argv[1]);
+    unsigned long long k = atoll(argv[2]);
+    unsigned long long local_sum = 0, global_sum = 0;
+
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    unsigned long long threshold = ceil(sqrtl(r * r / 2));
+    unsigned long long tmp = 0;
+
+    for (unsigned long long x = rank; x < r; x += size) {
+        tmp = ceil(sqrtl((r + x) * (r - x)));
+        if (tmp <= threshold) break;
+        local_sum += tmp - threshold;
+    }
+    local_sum %= k;
+    MPI_Reduce(&local_sum, &global_sum, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0,
+               MPI_COMM_WORLD);
+
+    if (rank == 0)
+        printf("%llu\n",
+               (4 * (global_sum * 2 + (threshold * threshold) % k) % k) % k);
+    MPI_Finalize();
 }
