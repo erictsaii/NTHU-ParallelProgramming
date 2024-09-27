71bc35e3ea6191c75ca3dfd6ee24713d09df9e8d
erictsaii <erictsai90@gmail.com>
2024-09-27 17:37:47 +0800

    feat: complete omp version

--- lab2/lab2_omp.cc
+++ lab2/lab2_omp.cc
@@ -8,11 +8,14 @@ int main(int argc, char** argv) {
     unsigned long long rr = r * r;
     unsigned long long k = atoll(argv[2]);
     unsigned long long pixels = 0;
+
 #pragma omp parallel for reduction(+ : pixels)
     for (unsigned long long x = 0; x < r; x++) {
         unsigned long long y = ceil(sqrtl(rr - x * x));
         pixels += y;
     }
+
     pixels %= k;
     printf("%llu\n", (4 * pixels) % k);
+    return 0;
 }
