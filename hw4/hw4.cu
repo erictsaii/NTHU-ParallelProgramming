f82f3606276b9af1cfe0e6501d46589a10bc12da
erictsaii <erictsai90@gmail.com>
2024-11-26 22:28:50 +0800

    feat: complete hw4 v3

--- hw4/hw4.cu
+++ hw4/hw4.cu
@@ -11,9 +11,7 @@
 
 void input(char *input_filename);
 void output(char *output_filename);
-void launch_flash_attention(float *q, float *k, float *v, float *o);
-
-__global__ void flash_attention(float *q, float *k, float *v, float *o, int d, int tc);
+__global__ void flash_attention(float *q, float *k, float *v, float *o, int d, int tc, int N);
 
 int B, N, d;
 float *Q, *K, *V, *O;
@@ -21,14 +19,36 @@ float *Q, *K, *V, *O;
 int main(int argc, char *argv[]) {
     input(argv[1]);
 
-    for (int i = 0; i < B; i++) {
-        launch_flash_attention(
-            Q + (i * N * d), 
-            K + (i * N * d), 
-            V + (i * N * d), 
-            O + (i * N * d)
-        );
-    }
+    int tr = N / br, tc = N / bc;
+    float *d_q, *d_k, *d_v, *d_o;
+    size_t BND = B * N * d * sizeof(float);
+
+    // Q
+    cudaHostRegister(Q, BND, cudaHostRegisterDefault);
+    cudaMalloc(&d_q, BND);
+    cudaMemcpy(d_q, Q, BND, cudaMemcpyHostToDevice);
+    // K
+    cudaHostRegister(K, BND, cudaHostRegisterDefault);
+    cudaMalloc(&d_k, BND);
+    cudaMemcpy(d_k, K, BND, cudaMemcpyHostToDevice);
+    // V
+    cudaHostRegister(V, BND, cudaHostRegisterDefault);
+    cudaMalloc(&d_v, BND);
+    cudaMemcpy(d_v, V, BND, cudaMemcpyHostToDevice);
+    // O
+    // cudaHostRegister(O, BND, cudaHostRegisterDefault);
+    cudaMalloc(&d_o, BND);
+    // cudaMemcpy(d_o, O, BND, cudaMemcpyHostToDevice);
+    
+    // grid size and block size
+    dim3 grid_size(tr, B);
+    dim3 block_size(32, 32); // 32 * 32 threads
+
+    // kernel function
+    flash_attention<<<grid_size, block_size>>>(d_q, d_k, d_v, d_o, d, tc, N);
+
+    // copy the output to host
+    cudaMemcpy(O, d_o, BND, cudaMemcpyDeviceToHost);
 
     output(argv[2]);
 
@@ -63,34 +83,8 @@ void input(char *input_filename) {
     fclose(file);
 }
 
-void launch_flash_attention(float *q, float *k, float *v, float *o) {
-    int tr = N / br, tc = N / bc;
-    float *d_q, *d_k, *d_v, *d_o;
-    // Q
-    cudaMalloc(&d_q, N*d*sizeof(float));
-    cudaMemcpy(d_q, q, N*d*sizeof(float), cudaMemcpyHostToDevice);
-    // K
-    cudaMalloc(&d_k, N*d*sizeof(float));
-    cudaMemcpy(d_k, k, N*d*sizeof(float), cudaMemcpyHostToDevice);
-    // V
-    cudaMalloc(&d_v, N*d*sizeof(float));
-    cudaMemcpy(d_v, v, N*d*sizeof(float), cudaMemcpyHostToDevice);
-    // O
-    cudaMalloc(&d_o, N*d*sizeof(float));
-    cudaMemcpy(d_o, o, N*d*sizeof(float), cudaMemcpyHostToDevice);
-    
-    // grid size and block size
-    dim3 grid_size(tr);
-    dim3 block_size(32, 32); // 32 * 32 threads
 
-    // kernel function
-    flash_attention<<<grid_size, block_size>>>(d_q, d_k, d_v, d_o, d, tc);
-
-    // copy the output to host
-    cudaMemcpy(o, d_o, N*d*sizeof(float), cudaMemcpyDeviceToHost);
-}
-
-__global__ void flash_attention(float *q, float *k, float *v, float *o, int d, int tc) {
+__global__ void flash_attention(float *q, float *k, float *v, float *o, int d, int tc, int N) {
     __shared__ float kj[bc * d_max]; 
     __shared__ float vj[bc * d_max];
     __shared__ float qi[br * d_max];
@@ -100,7 +94,7 @@ __global__ void flash_attention(float *q, float *k, float *v, float *o, int d, i
     __shared__ float pij[br * bc];
 
     float mij = 3.0;
-    int qo_offset = blockIdx.x * br * d;
+    int qo_offset = blockIdx.y * N * d + blockIdx.x * br * d;
     float sqrt_d = 1.0 / sqrtf(d);
     float pv = 0.0F;
     float tmp;
@@ -124,7 +118,7 @@ __global__ void flash_attention(float *q, float *k, float *v, float *o, int d, i
     // start for-loop
     for (int j = 0; j < tc; ++j) {
         // load kj 
-        int kjvj_offset = j * bc * d;
+        int kjvj_offset = blockIdx.y * N * d + j * bc * d;
         for (int i = 0; i < d; i += 32) {
             kj[threadIdx.y * d + threadIdx.x + i] = k[kjvj_offset + threadIdx.y * d + threadIdx.x + i];
         }
